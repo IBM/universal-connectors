a81cf273c81487a39b8a27142a2ea84cb83d4eb8
Rasika Shete <Rasika.Shete@ibm.com>
2021-06-23 19:54:24 +0530

    Dynamodb UC Plugin (#15)
    * Dynamodb Universal connector plugin
    
    * Deleted the folder
    
    * Dynamodb universal connector plugin
    
    * Corrected the data for some pre-defined files.
    
    * Redacted values are masked
    
    * Added the specified version
    
    Co-authored-by: Ofer Haim <Ofer.Haim@ibm.com>

---
+++ filter-plugin/logstash-filter-dynamodb-guardium/src/main/java/com/ibm/guardium/dynamodb/Constants.java
@@ -0,0 +1,39 @@
+package com.ibm.guardium.dynamodb;
+
+public interface Constants {
+	
+	public static final String NOT_AVAILABLE = "NA";
+	public static final String DATA_PROTOCOL_STRING = "DynamoDB native audit";
+	public static final String UNKNOWN_STRING = "";
+	public static final String SERVER_TYPE_STRING = "DynamoDB";
+    public static final String LOGSTASH_TAG_JSON_PARSE_ERROR = "_dynamoguardium_json_parse_error";
+
+	
+	public static final String REQUEST_ID = "requestID";
+	public static final String ERROR_MESSAGE = "errorMessage";
+	public static final String ERROR_CODE = "errorCode";
+	public static final String USER_IDENTITY = "userIdentity";
+	public static final String USERNAME = "userName";
+	public static final String SESSION_CONTEXT= "sessionContext";
+	public static final String SESSION_ISSUER = "sessionIssuer";
+	public static final String USER_AGENT = "userAgent";
+	public static final String SOURCE_IP_ADDRESS = "sourceIPAddress";
+	public static final String TABLE_NAME = "tableName";
+	public static final String EVENT_NAME = "eventName";
+	public static final String EVENT_TIME = "eventTime";
+	public static final String REQUEST_PARAMETERS = "requestParameters";
+	public static final String OBJECT_TYPE = "table"; //for dynamodb
+	public static final String PRINCIPAL_ID = "principalId";
+	public static final String ACCOUNT_ID = "accountId";
+	public static final String ACCESS_KEY_ID = "accessKeyId";
+	public static final String EVENT_ID = "eventId";
+	public static final String EVENT_SOURCE = "eventSource";
+
+	//constants
+	public static final int CLIENT_PORT = -1;
+	public static final String SERVER_IP = "0.0.0.0";
+	public static final int SERVER_PORT = -1;
+	public static final String LOGSTASH_TAG_SKIP_NOT_DYNAMO = "_dynamoguardium_skip_not_dynamodb";
+	public static final String MASK_STRING = "?";
+	
+}
