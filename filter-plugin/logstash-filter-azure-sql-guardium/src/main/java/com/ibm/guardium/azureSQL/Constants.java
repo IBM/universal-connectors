7ee5cff95054d2ba853759004f2439c6e21b7a7e
Laxmikant Mahale <Laxmikant.Mahale@ibm.com>
2022-05-12 19:15:39 +0530

    AzureSQL UC commit
    Signed-off-by: Laxmikant Mahale <Laxmikant.Mahale@ibm.com>

---
+++ filter-plugin/logstash-filter-azure-sql-guardium/src/main/java/com/ibm/guardium/azureSQL/Constants.java
@@ -0,0 +1,29 @@
+package com.ibm.guardium.azureSQL;
+
+public interface Constants {
+	
+	public static final String APP_USER_NAME = "AzureService";
+	
+	public static final String TIMESTAMP = "TIMESTAMP";
+	public static final String CLIENT_HOST_NAME = "CLIENT_HOST_NAME";
+	public static final String STATEMENT = "STATEMENT";
+	public static final String Client_IP = "Client_IP";
+	public static final String SERVER_INSTANCE_NAME = "SERVER_INSTANCE_NAME";
+	public static final String SUCCEEDED = "SUCCEEDED";
+	public static final String DATABASE_NAME = "DATABASE_NAME";
+	public static final String User_Name = "User_Name";
+	public static final String APPLICATION_NAME = "APPLICATION_NAME";
+	public static final String Server_Hostname = "Server_Hostname";
+	public static final String Session_ID = "Session_ID";
+	public static final String LOGSTASH_TAG_JSON_PARSE_ERROR = "_azureSQLguardium_json_parse_error";
+	public static final String DEFAULT_IP = "0.0.0.0";
+	public static final int DEFAULT_PORT = -1;
+	public static final String UNKNOWN_STRING = "";
+	public static final String SERVER_TYPE_STRING = "MSSQL";
+	public static final String DATA_PROTOCOL_STRING = "AzureSQL native audit";
+	public static final String LANGUAGE="MSSQL";
+	public static final String SQL_ERROR = "SQL_ERROR";
+	public static final String ZERO = "0";
+	public static final String ADDITIONAL_INFORMATION = "ADDITIONAL_INFORMATION";
+	
+}
