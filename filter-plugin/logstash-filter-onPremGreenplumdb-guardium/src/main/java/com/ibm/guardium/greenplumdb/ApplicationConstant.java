a02c59b55191530e4e8da0696d8b7e8f68e46d18
shivamibm <111949396+shivamibm@users.noreply.github.com>
2022-09-29 21:34:53 +0530

    Add Greenplum plug-in (#195)
    * add Greenplum plug-in
    
    * changes according to PR comments
    
    Signed-off-by: shivam agarwal <Shivam.Agarwal1@ibm.com>

---
+++ filter-plugin/logstash-filter-onPremGreenplumdb-guardium/src/main/java/com/ibm/guardium/greenplumdb/ApplicationConstant.java
@@ -0,0 +1,56 @@
+/*
+© Copyright IBM Corp. 2021, 2022 All rights reserved.
+SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.ibm.guardium.greenplumdb;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+public class ApplicationConstant {
+
+	public static final String SOURCE = "source";
+	public static final String MESSAGE = "mes***REMOVED***ge";
+
+	public static final String TIMESTAMP = "event_time";
+	public static final String PROCESS_ID = "process_id";
+	public static final String CONNECTION_ID = "gp_session_id";
+	public static final String QUERY = "debug_query_string";
+	public static final String POSTGRES = "postgres.c";
+	public static final String NBTINSERT = "nbtinsert.c";
+	public static final String AUTH = "auth.c";
+	public static final String REMOTE_HOST = "remote_host";
+	public static final String REMOTE_PORT = "remote_port";
+	public static final String USER_NAME = "user_name";
+	public static final String DATABASE_NAME = "database_name";
+	public static final String SERVER_HOSTNAME = "server_hostname";
+	public static final String EVENT_SEVERITY = "event_severity";
+	public static final String LANGUAGE = "GPLUM";
+	public static final String NOT_AVAILABLE = "NA";
+	public static final String DB_PROTOCOL = "GREENPLUMDB";
+	public static final String SERVER_TYPE = "GreenplumDB";
+	public static final String DEFAULT_IPV4 = "0.0.0.0";
+	public static final String DEFAULT_IPV6 = "0000:0000:0000:0000:0000:FFFF:0000:0000";
+	public static final String DEFAULT_PORT = "-1";
+	public static final String EMPTY = StringUtils.EMPTY;
+	public static final String EXCEPTION_TYPE_AUTHENTICATION_STRING = "LOGIN_FAILED";
+	public static final String EXCEPTION_TYPE_AUTHORIZATION_STRING = "SQL_ERROR";
+	public static final String ELOG = "elog.c";
+	public static final String ERROR = "ERROR";
+	public static final String FILE_NAME = "file_name";
+	public static final String STATE_CODE = "sql_state_code";
+	public static final String EVENT_MESSAGE = "event_mes***REMOVED***ge";
+	public static final String COLON = ":";
+	public static final String TRANSACTION_ID = "tran***REMOVED***ction_id";
+	public static final String SLICE_ID = "slice_id";
+	public static final String SLICE = "slice1";
+	public static final String SERVER_IP = "server_ip";
+	public static final String DOUBLE_QUOTE = "\"";
+	public static final String LOCAL = "local";
+	
+	
+	
+	
+
+}
