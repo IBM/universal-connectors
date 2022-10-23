8c293f63ecf48e32a952cf64535d887dc91bfe8c
Refael Adi <Refael.Adi@ibm.com>
2022-10-23 13:52:52 +0300

    Adjustments for SaaS: 1. changed the gradle version to 6.5.1 2. fixed the encoding error

--- filter-plugin/logstash-filter-azure-apachesolr-guardium/src/main/java/com/ibm/guardium/apachesolrdb/ApplicationConstant.java
+++ filter-plugin/logstash-filter-azure-apachesolr-guardium/src/main/java/com/ibm/guardium/apachesolrdb/ApplicationConstant.java
@@ -1,7 +1,7 @@
 /*
-© Copyright IBM Corp. 2021, 2022 All rights reserved.
-SPDX-License-Identifier: Apache-2.0
-*/
+ * ï¿½ Copyright IBM Corp. 2021, 2022 All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package com.ibm.guardium.apachesolrdb;
 
 import org.apache.commons.lang3.StringUtils;
