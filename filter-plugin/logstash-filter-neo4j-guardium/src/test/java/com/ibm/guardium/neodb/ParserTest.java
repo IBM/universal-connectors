3ab62ad573fdb28d84d0b721567a195ba2d84fe4
Refael Adi <Refael.Adi@ibm.com>
2023-02-15 20:21:48 +0200

    remove uses of Grok library and hard coded parse the sample events before testing the parser.

--- filter-plugin/logstash-filter-neo4j-guardium/src/test/java/com/ibm/guardium/neodb/ParserTest.java
+++ filter-plugin/logstash-filter-neo4j-guardium/src/test/java/com/ibm/guardium/neodb/ParserTest.java
@@ -5,40 +5,43 @@
 
 package com.ibm.guardium.neodb;
 
-import java.util.Map;
-
-import org.aicer.grok.dictionary.GrokDictionary;
-import org.aicer.grok.util.Grok;
+import co.elastic.logstash.api.Event;
+import com.google.gson.JsonObject;
+import com.ibm.guardium.univer***REMOVED***lconnector.commons.structures.*;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.google.gson.JsonObject;
-import com.ibm.guardium.univer***REMOVED***lconnector.commons.structures.Accessor;
-import com.ibm.guardium.univer***REMOVED***lconnector.commons.structures.Construct;
-import com.ibm.guardium.univer***REMOVED***lconnector.commons.structures.ExceptionRecord;
-import com.ibm.guardium.univer***REMOVED***lconnector.commons.structures.Sentence;
-import com.ibm.guardium.univer***REMOVED***lconnector.commons.structures.SessionLocator;
-import com.ibm.guardium.univer***REMOVED***lconnector.commons.structures.Time;
-
-import co.elastic.logstash.api.Event;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ParserTest {
 
-	    Parser parser = new Parser();
 	    final String neoSuccessString = "2021-08-06 17:09:40.008+0000 INFO  9 ms: (planning: 1, waiting: 0) - 0 B - 4 page hits, 0 page faults - bolt-session	bolt	neo4j-browser/v4.3.1		client/127.0.0.1:51372	server/127.0.0.1:11004>	neo4j - neo4j - MATCH (Ishant:player {name: \"Ishant Sharma\", YOB: 1988, POB: \"Delhi\"}) DETACH DELETE Ishant - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}";
-	    
-	    final String neoErrorString = "2021-01-25 11:17:09.099+0000 INFO  106 ms: (planning: 61, waiting: 0) - 0 B - 1 page hits, 0 page faults - bolt-session	bolt	neo4j-browser/v4.2.1		client/127.0.0.1:53010	server/127.0.0.1:7687>	<none> - neo4j - EXPLAIN MATCH(n) - {} - runtime=null - {type: 'user-action', app: 'neo4j-browser_v4.2.1'} - Query cannot conclude with MATCH (must be RETURN or an update clause) (line 1, column 9 (offset: 8))";
-	    
-	    final String skipString = "2021-08-06 14:09:07.781+0000 INFO  71 ms: (planning: 69, waiting: 0) - 64 B - 3 page hits, 0 page faults - bolt-session   bolt    neo4j-javascript/0.0.0-dev              client/127.0.0.1:53828  server/127.0.0.1:11004> guardiumdb - neo4j - MATCH ()-->() RETURN count(*); - {} - runtime=pipelined - {}";
-	    
+		final String neoSuccessString_grokOutput = "{\n" +
+				"    \"ts\": \"2021-08-06 17:09:40.008+0000\",\n" +
+				"    \"log_level\": \"INFO\",\n" +
+				"    \"metadata1\": \" 9 ms: (planning: 1, waiting: 0) - 0 B - 4 page hits, 0 page faults - bolt-session\",\n" +
+				"    \"protocol\": \"bolt\",\n" +
+				"    \"driverVersion\": \"neo4j-browser/v4.3.1\",\n" +
+				"    \"client_ip\": \"client/127.0.0.1:51372\",\n" +
+				"    \"server_ip\": \"server/127.0.0.1:11004\",\n" +
+				"    \"dbname\": \"neo4j \",\n" +
+				"    \"dbuser\": \"neo4j \",\n" +
+				"    \"queryStatement\": \"MATCH (Ishant:player {name: \\\"Ishant Sharma\\\", YOB: 1988, POB: \\\"Delhi\\\"}) DETACH DELETE Ishant - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}\"\n" +
+				"  }";
+
 	    @Test
 	    public void testParseAsConstruct_Match() {
 	    	
-	    	Event e = getParsedEvent(neoSuccessString);
+	    	Event e = getParsedEvent(neoSuccessString_grokOutput, neoSuccessString);
 	    	
 	    	JsonObject inputData = inputData(e);
-	        
-	        final Construct result = Parser.parseAsConstruct(inputData);
+			String arr[] = {"MATCH ", "Ishant:player {..."} ;
+	        int a = arr[1].indexOf(")");
+			int b = arr[1].indexOf(":");
+			String temp[] = arr[1].split("\\)", 2);
+
+			final Construct result = Parser.parseAsConstruct(inputData);
 	        
 	        final Sentence sentence = result.sentences.get(0);
 	        
@@ -50,7 +53,19 @@ public class ParserTest {
 	    @Test
 	    public void testParseAsConstruct_Create() {
 	        String neoString = "2021-08-06 15:57:11.502+0000 INFO  2 ms: (planning: 1, waiting: 0) - 1704 B - 0 page hits, 0 page faults - bolt-session	bolt	neo4j-browser/v4.3.1		client/127.0.0.1:54356	server/127.0.0.1:11004>	neo4j - neo4j - CREATE (friend:Person {name: 'Mark'})   RETURN friend - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}";
-	    	Event e = getParsedEvent(neoString);
+			String neoString_grokOutput = "{\n" +
+					"    \"ts\": \"2021-08-06 15:57:11.502+0000\",\n" +
+					"    \"log_level\": \"INFO\",\n" +
+					"    \"metadata1\": \" 2 ms: (planning: 1, waiting: 0) - 1704 B - 0 page hits, 0 page faults - bolt-session\",\n" +
+					"    \"protocol\": \"bolt\",\n" +
+					"    \"driverVersion\": \"neo4j-browser/v4.3.1\",\n" +
+					"    \"client_ip\": \"client/127.0.0.1:54356\",\n" +
+					"    \"server_ip\": \"server/127.0.0.1:11004\",\n" +
+					"    \"dbname\": \"neo4j \",\n" +
+					"    \"dbuser\": \"neo4j \",\n" +
+					"    \"queryStatement\": \"CREATE (friend:Person {name: 'Mark'})   RETURN friend - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}\"\n" +
+					"  }";
+	    	Event e = getParsedEvent(neoString_grokOutput,neoString);
 	    	
 	    	JsonObject inputData = inputData(e);
 	        
@@ -66,7 +81,19 @@ public class ParserTest {
 	    @Test
 	    public void testParseAsConstruct_Merge() {
 	        String neoString = "2021-08-06 15:56:12.097+0000 INFO  4 ms: (planning: 1, waiting: 0) - 0 B - 6 page hits, 0 page faults - bolt-session	bolt	neo4j-browser/v4.3.1		client/127.0.0.1:54356	server/127.0.0.1:11004>	neo4j - neo4j - MERGE (mark:Person {name: 'Mark'})   RETURN mark - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}";
-	    	Event e = getParsedEvent(neoString);
+			String neoString_grokOutput = "{\n" +
+					"    \"ts\": \"2021-08-06 15:56:12.097+0000\",\n" +
+					"    \"log_level\": \"INFO\",\n" +
+					"    \"metadata1\": \" 4 ms: (planning: 1, waiting: 0) - 0 B - 6 page hits, 0 page faults - bolt-session\",\n" +
+					"    \"protocol\": \"bolt\",\n" +
+					"    \"driverVersion\": \"neo4j-browser/v4.3.1\",\n" +
+					"    \"client_ip\": \"client/127.0.0.1:54356\",\n" +
+					"    \"server_ip\": \"server/127.0.0.1:11004\",\n" +
+					"    \"dbname\": \"neo4j \",\n" +
+					"    \"dbuser\": \"neo4j \",\n" +
+					"    \"queryStatement\": \"MERGE (mark:Person {name: 'Mark'})   RETURN mark - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}\"\n" +
+					"  }";
+	    	Event e = getParsedEvent(neoString_grokOutput,neoString);
 	    	
 	    	JsonObject inputData = inputData(e);
 	        
@@ -82,7 +109,19 @@ public class ParserTest {
 	    @Test
 	    public void testParseException_DELETE() {
 	        String neoString = "2021-03-03 08:49:32.367+0000 ERROR 7 ms: (planning: 7, waiting: 0) - 0 B - 0 page hits, 0 page faults - bolt-session	bolt	neo4j-browser/v4.2.1		client/127.0.0.1:62845	server/127.0.0.1:7687>	<none> - neo4j - DETACH DELETE node - {} - runtime=null - {type: 'user-action', app: 'neo4j-browser_v4.2.1'} - Variable `node` not defined (line 1, column 23 (offset: 22))";
-	    	Event e = getParsedEvent(neoString);
+			String neoString_grokOutput = "{\n" +
+					"    \"ts\": \"2021-03-03 08:49:32.367+0000\",\n" +
+					"    \"log_level\": \"ERROR\",\n" +
+					"    \"metadata1\": \"7 ms: (planning: 7, waiting: 0) - 0 B - 0 page hits, 0 page faults - bolt-session\",\n" +
+					"    \"protocol\": \"bolt\",\n" +
+					"    \"driverVersion\": \"neo4j-browser/v4.2.1\",\n" +
+					"    \"client_ip\": \"client/127.0.0.1:62845\",\n" +
+					"    \"server_ip\": \"server/127.0.0.1:7687\",\n" +
+					"    \"dbname\": \"<none> \",\n" +
+					"    \"dbuser\": \"neo4j \",\n" +
+					"    \"queryStatement\": \"DETACH DELETE node - {} - runtime=null - {type: 'user-action', app: 'neo4j-browser_v4.2.1'} - Variable `node` not defined (line 1, column 23 (offset: 22))\\\";\"\n" +
+					"  }";
+			Event e = getParsedEvent(neoString_grokOutput, neoString);
 
 	    	JsonObject inputData = inputData(e);
 	        
@@ -90,13 +129,12 @@ public class ParserTest {
 
 	        Assert.assertEquals("Variable `node` not defined (line 1, column 23 (offset: 22))", exceptionRecord.getDescription().trim());
 	        Assert.assertEquals("DETACH DELETE node - {} - runtime=null - {type: 'user-action', app: 'neo4j-browser_v4.2.1", exceptionRecord.getSqlString().trim());
-	        
 	    }
 	    
 	    @Test
 	    public void testParseAccessor() {
 
-	    	Event e = getParsedEvent(neoSuccessString);
+	    	Event e = getParsedEvent(neoSuccessString_grokOutput,neoSuccessString);
 	    	
 	    	JsonObject inputData = inputData(e);
 	        
@@ -106,14 +144,12 @@ public class ParserTest {
 	        Assert.assertEquals("NEO4J", accessor.getServerType().toString().trim());
 	        Assert.assertEquals("neo4j", accessor.getDbUser().toString().trim());
 	        Assert.assertEquals("FREE_TEXT", accessor.getLanguage().toString().trim());
-	        
-	        
 	    }
 	    
 	    @Test
 	    public void testParseSessionLocator() {
 
-	    	Event e = getParsedEvent(neoSuccessString);
+	    	Event e = getParsedEvent(neoSuccessString_grokOutput, neoSuccessString);
 	    	
 	    	JsonObject inputData = inputData(e);
 	        
@@ -129,7 +165,7 @@ public class ParserTest {
 	    @Test
 	    public void testParseTimestamp() {
 
-	    	Event e = getParsedEvent(neoSuccessString);
+	    	Event e = getParsedEvent(neoSuccessString_grokOutput, neoSuccessString);
 	    	e.setField(Constants.TIMESTAMP, "2021-01-25 11:17:09.099+0000");
 	    	JsonObject inputData = inputData(e);
 	    	
@@ -155,7 +191,7 @@ public class ParserTest {
 	    @Test
 	    public void testParseSentence() {
 
-	    	Event e = getParsedEvent(neoSuccessString);
+	    	Event e = getParsedEvent(neoSuccessString_grokOutput, neoSuccessString);
 	    	
 	    	JsonObject inputData = inputData(e);
 	        
@@ -170,7 +206,7 @@ public class ParserTest {
 	    @Test
 	    public void testParseRedactedSensitiveDataSql() {
 	    	
-	    	Event e = getParsedEvent(neoSuccessString);
+	    	Event e = getParsedEvent(neoSuccessString_grokOutput, neoSuccessString);
 	    	
 	    	JsonObject inputData = inputData(e);
 	        
@@ -217,42 +253,58 @@ public class ParserTest {
 			}
 			return data;
 		}
-	    
-	    public static Event getParsedEvent(String logEvent){
-	    	
-	    	Map<String, String> results = getGrokParsedEvent(logEvent);
-	    	
-	    	Event e = new org.logstash.Event();
-	    	e.setField("mes***REMOVED***ge", logEvent);
-	    	
-			e.setField(Constants.CLIENT_IP, results.get(Constants.CLIENT_IP));
-			e.setField(Constants.SERVER_IP, results.get(Constants.SERVER_IP));
-			e.setField(Constants.DB_PROTOCOL, results.get(Constants.DB_PROTOCOL));
-			e.setField(Constants.TIMESTAMP, results.get(Constants.TIMESTAMP));
-			e.setField(Constants.LOG_LEVEL, results.get(Constants.LOG_LEVEL));
-			e.setField(Constants.DB_USER, results.get(Constants.DB_USER));
-			e.setField(Constants.DB_NAME, results.get(Constants.DB_NAME));
-			e.setField(Constants.SOURCE_PROGRAM, results.get(Constants.SOURCE_PROGRAM));
-			e.setField(Constants.QUERY_STATEMENT, results.get(Constants.QUERY_STATEMENT));
 
-	    	return e;
-	    	
-	    }
-	    
-	    private static Map<String, String> getGrokParsedEvent(String logEvent){
-	    	
-			final String expression = "(?<ts>[^[A-Z]]*) %{LOGLEVEL:log_level}\\s(?<metadata1>[^\\\t]*)\t(?<protocol>[^ \\s]*)\\s(?<driverVersion>[^ \t]*)\t\t(?<client_ip>[^ \t]*)\t(?<server_ip>[^ >]*)\\>\t(?<dbname>[^-]*)\\-\\s(?<dbuser>[^-]*)\\- %{GREEDYDATA:queryStatement}";
-			final GrokDictionary dictionary = new GrokDictionary();
-			
-			// Load the built-in dictionaries
-			dictionary.addBuiltInDictionaries();
-			
-			// Resolve all expressions loaded
-			dictionary.bind();
-			
-			Grok compiledPattern = dictionary.compileExpression(expression);
-			
-			return compiledPattern.extractNamedGroups(logEvent);
-	  }
 
+	public static Event getParsedEvent(String logEvent_json , String logEvent) {
+
+		Map<String,String> results = jsonToMap(logEvent_json);
+		Event e = new org.logstash.Event();
+		e.setField("mes***REMOVED***ge", logEvent);
+
+		e.setField(Constants.CLIENT_IP, results.get(Constants.CLIENT_IP));
+		e.setField(Constants.SERVER_IP, results.get(Constants.SERVER_IP));
+		e.setField(Constants.DB_PROTOCOL, results.get(Constants.DB_PROTOCOL));
+		e.setField(Constants.TIMESTAMP, results.get(Constants.TIMESTAMP));
+		e.setField(Constants.LOG_LEVEL, results.get(Constants.LOG_LEVEL));
+		e.setField(Constants.DB_USER, results.get(Constants.DB_USER));
+		e.setField(Constants.DB_NAME, results.get(Constants.DB_NAME));
+		e.setField(Constants.SOURCE_PROGRAM, results.get(Constants.SOURCE_PROGRAM));
+		e.setField(Constants.QUERY_STATEMENT, results.get(Constants.QUERY_STATEMENT));
+
+		return e;
+	}
+
+	public static Map<String, String> jsonToMap(String json) {
+		Map<String, String> result = new HashMap<>();
+
+		for (String key : getKeys(json)) {
+			String value = getValue(json, key);
+			result.put(key, value);
+		}
+
+		return result;
+	}
+
+	private static String getValue(String json, String key) {
+		String pattern = String.format("\"%s\":\\s*\"?([^\",\\]}]*)\"?", key);
+		java.util.regex.Pattern p = java.util.regex.Pattern.compile(pattern);
+		java.util.regex.Matcher m = p.matcher(json);
+		if (m.find()) {
+			return m.group(1);
+		}
+		return null;
+	}
+
+	private static java.util.List<String> getKeys(String json) {
+		String pattern = "\"([^\"]+)\":";
+		java.util.regex.Pattern p = java.util.regex.Pattern.compile(pattern);
+		java.util.regex.Matcher m = p.matcher(json);
+		java.util.List<String> keys = new java.util.ArrayList<>();
+		while (m.find()) {
+			keys.add(m.group(1));
+		}
+		return keys;
+	}
 }
+
+
