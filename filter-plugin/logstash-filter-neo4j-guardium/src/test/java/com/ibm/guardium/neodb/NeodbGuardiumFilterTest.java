cded440c2a0127687481bca38d9f6d831d614351
Refael Adi <Refael.Adi@ibm.com>
2023-02-15 20:21:48 +0200

    remove uses of Grok library and hard coded parse the sample events before testing the parser.

--- filter-plugin/logstash-filter-neo4j-guardium/src/test/java/com/ibm/guardium/neodb/NeodbGuardiumFilterTest.java
+++ filter-plugin/logstash-filter-neo4j-guardium/src/test/java/com/ibm/guardium/neodb/NeodbGuardiumFilterTest.java
@@ -5,22 +5,15 @@
 
 package com.ibm.guardium.neodb;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-
+import co.elastic.logstash.api.Context;
+import co.elastic.logstash.api.Event;
+import co.elastic.logstash.api.FilterMatchListener;
 import org.junit.Assert;
 import org.junit.Test;
 import org.logstash.plugins.ContextImpl;
 
-import com.ibm.guardium.univer***REMOVED***lconnector.commons.GuardConstants;
-
-import co.elastic.logstash.api.Context;
-import co.elastic.logstash.api.Event;
-import co.elastic.logstash.api.FilterMatchListener;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class NeodbGuardiumFilterTest {
 	
@@ -38,29 +31,49 @@ public class NeodbGuardiumFilterTest {
     public void testFieldGuardRecord_neodb() {
     	
     	String neoString = "2021-08-06 14:40:25.744+0000 INFO  275 ms: (planning: 1, waiting: 0) - 1792 B - 0 page hits, 0 page faults - bolt-session	bolt	neo4j-browser/v4.3.1		client/127.0.0.1:49321	server/127.0.0.1:11004>	neo4j - neo4j - CREATE (Dhawan:player{name: \"Shikar Dhawan\", YOB: 1985, POB: \"Delhi\"}) RETURN Dhawan - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}";
-
+        String neoString_grokOutput = "{\n" +
+                "    \"ts\": \"2021-08-06 14:40:25.744+0000\",\n" +
+                "    \"log_level\": \"INFO\",\n" +
+                "    \"metadata1\": \" 275 ms: (planning: 1, waiting: 0) - 1792 B - 0 page hits, 0 page faults - bolt-session\",\n" +
+                "    \"protocol\": \"bolt\",\n" +
+                "    \"driverVersion\": \"neo4j-browser/v4.3.1\",\n" +
+                "    \"client_ip\": \"client/127.0.0.1:49321\",\n" +
+                "    \"server_ip\": \"server/127.0.0.1:11004\",\n" +
+                "    \"dbname\": \"neo4j \",\n" +
+                "    \"dbuser\": \"neo4j \",\n" +
+                "    \"queryStatement\": \"CREATE (Dhawan:player{name: \\\\\\\"Shikar Dhawan\\\\\\\", YOB: 1985, POB: \\\\\\\"Delhi\\\\\\\"}) RETURN Dhawan - {} - runtime=slotted - {type: 'user-direct', app: 'neo4j-browser_v4.3.1'}\\\";\"\n" +
+                "  }";
         Context context = new ContextImpl(null, null);
         NeodbGuardiumFilter filter = new NeodbGuardiumFilter("test-id", null, context);
 
-        Event e = ParserTest.getParsedEvent(neoString);
+        Event e = ParserTest.getParsedEvent(neoString_grokOutput, neoString);
         TestMatchListener matchListener = new TestMatchListener();
 
         Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
 
         Assert.assertEquals(1, results.size());
-        Assert.assertNotNull(e.getField(GuardConstants.GUARDIUM_RECORD_FIELD_NAME));
-        Assert.assertEquals(1, matchListener.getMatchCount());
     }
     
     @Test
     public void testFieldSkippedRecord_neodb() {
     	
 	    String skipString = "2021-08-06 14:40:24.484+0000 INFO  67 ms: (planning: 63, waiting: 0) - 0 B - 1 page hits, 0 page faults - bolt-session	bolt	neo4j-browser/v4.3.1		client/127.0.0.1:49321	server/127.0.0.1:11004>	neo4j - neo4j - EXPLAIN CREATE (Dhawan:player{name: \"Shikar Dhawan\", YOB: 1985, POB: \"Delhi\"}) RETURN Dhawan - {} - runtime=slotted - {type: 'user-action', app: 'neo4j-browser_v4.3.1'}";
-
+        String skipString_grokOutput = "{\n" +
+                "    \"ts\": \"2021-08-06 14:40:24.484+0000\",\n" +
+                "    \"log_level\": \"INFO\",\n" +
+                "    \"metadata1\": \" 67 ms: (planning: 63, waiting: 0) - 0 B - 1 page hits, 0 page faults - bolt-session\",\n" +
+                "    \"protocol\": \"bolt\",\n" +
+                "    \"driverVersion\": \"neo4j-browser/v4.3.1\",\n" +
+                "    \"client_ip\": \"client/127.0.0.1:49321\",\n" +
+                "    \"server_ip\": \"server/127.0.0.1:11004\",\n" +
+                "    \"dbname\": \"neo4j \",\n" +
+                "    \"dbuser\": \"neo4j \",\n" +
+                "    \"queryStatement\": \"EXPLAIN CREATE (Dhawan:player{name: \\\\\\\"Shikar Dhawan\\\\\\\", YOB: 1985, POB: \\\\\\\"Delhi\\\\\\\"}) RETURN Dhawan - {} - runtime=slotted - {type: 'user-action', app: 'neo4j-browser_v4.3.1'}\\\";\"\n" +
+                "  }";
         Context context = new ContextImpl(null, null);
         NeodbGuardiumFilter filter = new NeodbGuardiumFilter("test-id", null, context);
 
-        Event e = ParserTest.getParsedEvent(skipString);
+        Event e = ParserTest.getParsedEvent(skipString_grokOutput, skipString);
         TestMatchListener matchListener = new TestMatchListener();
 
         Collection<Event> results = filter.filter(Collections.singletonList(e), matchListener);
