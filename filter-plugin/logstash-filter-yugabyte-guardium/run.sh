dbdf4680f7ad6bd0bcb1faad517c4a96a3856fe8
Chirag Soni <chirag.soni@ibm.com>
2023-03-01 13:25:12 +0530

    fix: removed unwanted directories and files
    Signed-off-by: Chirag Soni <chirag.soni@ibm.com>

--- filter-plugin/logstash-filter-yugabyte-guardium/run.sh
+++
@@ -1,9 +0,0 @@
-if [ -n "$LOGSTASH_HOME" ]; then
-  echo "building the plugin......"
-  ./gradlew gem
-  echo "installing the plugin...."
-  VERSION=`cat ./VERSION`
-  $LOGSTASH_HOME/bin/logstash-plugin install --no-verify --local ./logstash-filter-yugabytedb_guardium_filter-$VERSION.gem
-else
-  echo "LOGSTASH_HOME is not defined."
-fi
--- filter-plugin/logstash-filter-yugabyte-guardium/vendor/jruby/lib/ruby/gems/shared/gems/did_you_mean-1.2.1/lib/did_you_mean/formatters/plain_formatter.rb
+++
@@ -1,9 +0,0 @@
-# frozen-string-literal: true
-
-module DidYouMean
-  class PlainFormatter
-    def mes***REMOVED***ge_for(corrections)
-      corrections.empty? ? "" : "\nDid you mean?  #{corrections.join("\n               ")}"
-    end
-  end
-end
