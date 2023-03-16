407692e9766e7d86119b32933ab678bddedf6a94
Natalia Geimakher <94900027+natalygmkibm@users.noreply.github.com>
2023-03-16 11:31:35 +0200

    GRD-66595 - add proxy support to mongo atlas input plugin (#291)
    * GRD-66595 - add proxy support to mongo atlas input plugin
    
    Signed-off-by: NATALIYA GEIMAKHER <Nataliya.Geimakher@ibm.com>
    
    * GRD-66595 - update msg in case auditlogs are not available (not error)
    
    Signed-off-by: NATALIYA GEIMAKHER <Nataliya.Geimakher@ibm.com>
    
    * GRD-66595 - update binary
    
    Signed-off-by: NATALIYA GEIMAKHER <Nataliya.Geimakher@ibm.com>
    
    ---------
    
    Signed-off-by: NATALIYA GEIMAKHER <Nataliya.Geimakher@ibm.com>

---
+++ input-plugin/logstash-input-mongo-atlas/src/test/java/org/logstashplugins/JavaInputExampleTest.java
@@ -0,0 +1,58 @@
+
+package org.logstashplugins;
+
+import co.elastic.logstash.api.Configuration;
+import com.sun.javafx.collections.MappingChange;
+import org.junit.Test;
+
+import java.util.HashMap;
+import co.elastic.logstash.api.Configuration;
+import co.elastic.logstash.api.Event;
+import org.junit.Test;
+import org.logstash.plugins.ConfigurationImpl;
+
+import java.io.ByteArrayOutputStream;
+import java.util.*;
+import java.util.function.Consumer;
+
+public class JavaInputExampleTest{
+
+//    @Test
+    public void testActualPullingFromMongoAtlas() throws InterruptedException {
+
+        Map<String, Object> configValues = new HashMap<>();
+        configValues.put(MongoAtlasInput.INTERVAL_CONFIG.name(), 300L);
+        configValues.put(MongoAtlasInput.PRIVATE_KEY_CONFIG.name(),"abc");
+        configValues.put(MongoAtlasInput.PUBLIC_KEY_CONFIG.name(),"aaa-81d0-bbbb-ccc-a12345676789");
+        configValues.put(MongoAtlasInput.GROUP_ID_CONFIG.name(), "123456789abcefg");
+        configValues.put(MongoAtlasInput.HOSTNAME_CONFIG.name(), "cluster1-shard-12-34.i1234.mongodb.net");
+        configValues.put(MongoAtlasInput.TYPE_CONFIG.name(), "mongodbatlas");
+
+        Configuration config = new ConfigurationImpl(configValues);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        MongoAtlasInput inputSample = new MongoAtlasInput("test-id", config, null);
+
+        Consumer<Map<String, Object>> consumer = new Consumer<Map<String, Object>>() {
+            @Override
+            public void accept(Map<String, Object> stringObjectMap) {
+                System.out.println("Got results");
+                for (String s : stringObjectMap.keySet()) {
+                    System.out.println(" key "+s+", value "+stringObjectMap.get(s));
+                }
+            }
+        };
+
+        inputSample.start(consumer);
+
+        System.out.println("Started");
+
+    //
+
+    //            e.setField(GuardConstants.GUARDIUM_RECORD_FIELD_NAME, s3Record);
+    //            events.add(e);
+    //            output.output(events);
+
+        Thread.sleep(10000);
+    }
+
+}
