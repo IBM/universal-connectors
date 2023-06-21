1f1db4f5ce4d0307c41000fa22efeb2ee7e5559c
Refael Adi <refael.adi@ibm.com>
2023-06-21 17:53:55 +0300

    Prepare GI+GDP plugins

--- packagePluginsForGuardiumInsights.sh
+++ packagePluginsForGuardiumInsights.sh
@@ -5,28 +5,46 @@
 #---------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------
 
-VERIFIED_UC_PLUGINS_FILE="verifiedUCPlugins.txt"
+VERIFIED_UC_GDP_PLUGINS_FILE="verifiedUCPlugins_gdp.txt"
+VERIFIED_UC_GI_PLUGINS_FILE="verifiedUCPlugins_gi.txt"
+
 PACKAGED_PLUGINS_DIR=dist/plugins
+GI_PLUGINS_DIR=dist/temp
+PLUGINS_LISTS_DIR=build/
 originalPath=$(pwd)
 
 function zipPackage {
-  zipName="$(echo $1 | awk -F"\/" '{print $NF}')"
+  zipName="$(echo $1 | awk -F'/' '{print $NF}')"
   packageFolder=$(dirname $1)
+  targetDirectory=$2
 
-  cd $packageFolder
-  echo "location to folder to package: $(pwd)"
+  cd "$packageFolder"
+  echo "location of folder to package: $(pwd)"
   echo "folder to package: ${zipName}"
-  zip -r "${originalPath}/${PACKAGED_PLUGINS_DIR}/${zipName}.zip" $zipName -x "*.zip"
-  cd $originalPath
+  zip -r "${originalPath}/${targetDirectory}/${zipName}.zip" $zipName -x "*.zip"
+  cd "$originalPath"
 }
 
 mkdir -p ${PACKAGED_PLUGINS_DIR};
-grep -v '^#' $VERIFIED_UC_PLUGINS_FILE | while read -r line ; do zipPackage "$line" ; done
+mkdir -p ${GI_PLUGINS_DIR}
 
-# list all plugins into a file
-cd ${PACKAGED_PLUGINS_DIR}
+# go to the build direction and zip all the plug-ins packages (the GDP plugins list includes the both GDP+GI plugins)
+cd ${PLUGINS_LISTS_DIR}
+grep -v '^#' ${VERIFIED_UC_GDP_PLUGINS_FILE} | while read -r line ; do zipPackage "$line" "${PACKAGED_PLUGINS_DIR}"; done
+
+# zip the GI plugins to a temp location
+grep -v '^#' ${VERIFIED_UC_GI_PLUGINS_FILE} | while read -r line ; do zipPackage "$line" "${GI_PLUGINS_DIR}"; done
+
+# list all GI plugins into a file
+cd ${originalPath}/${GI_PLUGINS_DIR}
 ls | grep -v '/$' | grep -v 'plugins_list.txt' > "plugins_list.txt"
 
+
 # zip all plugins zips and the names list into one zip
-cd $originalPath
-zip -j -r "plugins.zip" "${PACKAGED_PLUGINS_DIR}"
+cd ${originalPath}
+zip -j -r "gdp_plugins.zip" "${PACKAGED_PLUGINS_DIR}"
+zip -j -r "plugins.zip" "${GI_PLUGINS_DIR}"
+
+# remove temporary direction
+rm -r ${originalPath}/${GI_PLUGINS_DIR}
+
