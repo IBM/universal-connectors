f4dcf9146f1db5d8fcc1e916a49d2534efa4a8ba
Refael Adi <Refael.Adi@ibm.com>
2023-07-31 09:13:17 +0300

    zip also the offline plugins

--- build/packagePluginsForGuardiumInsights.sh
+++ build/packagePluginsForGuardiumInsights.sh
@@ -12,19 +12,36 @@ PACKAGED_PLUGINS_DIR=dist/plugins
 GI_PLUGINS_DIR=dist/temp
 PLUGINS_LISTS_DIR=build/
 
+
 GI_PLUGINS_TEMPLATES="gi_plugins_templates.zip"
 GDP_PLUGINS_TEMPLATES="gdp_plugins_templates.zip"
 originalPath=$(pwd)
 
 function zipPackage {
-  zipName="$(echo $1 | awk -F'/' '{print $NF}')"
-  packageFolder=$(dirname $1)
+  IFS=';' read -r package_folder offline_plugin <<< "$1"
+
+  echo "package_folder is: $package_folder"
+  if [ -n "$offline_plugin" ]; then
+    echo "offline_plugin is: $offline_plugin"
+  fi
+
+
+  zipName="$(echo $package_folder | awk -F'/' '{print $NF}')"
+  packageFolder=$(dirname $package_folder)
   targetDirectory=$2
 
   cd "${originalPath}/${packageFolder}"
   echo "location of folder to package: $(pwd)"
   echo "folder to package: ${zipName}"
   zip -r "${originalPath}/${targetDirectory}/${zipName}.zip" $zipName -x "*.zip"
+
+  # if there is also offline plugins, enter it to the zip:
+  if [ -e "$originalPath/build/offline_packages/zips/$offline_plugin" ]; then
+      echo "The file $offline_plugin exists."
+
+      zip -uj "${originalPath}/${targetDirectory}/${zipName}.zip" $originalPath/build/offline_packages/zips/$offline_plugin
+  fi
+
   cd "$originalPath"
 }
 
@@ -35,7 +52,7 @@ mkdir -p ${GI_PLUGINS_DIR}
 cd ${PLUGINS_LISTS_DIR}
 grep -v '^#' ${VERIFIED_UC_GDP_PLUGINS_FILE} | while read -r line ; do zipPackage "$line" "${PACKAGED_PLUGINS_DIR}" ; done
 
-# zip the GI plugins to a temp location
+#zip the GI plugins to a temp location
 grep -v '^#' ${VERIFIED_UC_GI_PLUGINS_FILE} | while read -r line ; do zipPackage "$line" "${GI_PLUGINS_DIR}" ; done
 
 # list all GI plugins into a file
@@ -43,11 +60,10 @@ cd ${originalPath}/${GI_PLUGINS_DIR}
 ls | grep -v '/$' | grep -v 'plugins_list.txt' > "plugins_list.txt"
 
 
-# zip all plugins zips and the names list into one zip
+#zip all plugins zips and the names list into one zip
 cd ${originalPath}
 zip -j -r "${GDP_PLUGINS_TEMPLATES}" "${PACKAGED_PLUGINS_DIR}"
 zip -j -r "${GI_PLUGINS_TEMPLATES}" "${GI_PLUGINS_DIR}"
 
 # remove the temporary direction
 rm -r ${originalPath}/${GI_PLUGINS_DIR}
-
