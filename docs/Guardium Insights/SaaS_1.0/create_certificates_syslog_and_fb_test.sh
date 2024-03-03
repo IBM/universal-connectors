91c4617be4cb065dc20fd9ec5520ffd22e9f97f5
Refael Adi <Refael.Adi@ibm.com>
2024-03-03 20:38:53 +0200

    Revert changes in the certificate script and create a new script file for testing
    Signed-off-by: Refael Adi <Refael.Adi@ibm.com>

--- docs/Guardium Insights/SaaS_1.0/create_certificates_syslog_and_fb_test.sh
+++ docs/Guardium Insights/SaaS_1.0/create_certificates_syslog_and_fb_test.sh
@@ -19,7 +19,7 @@ mkdir -p $CERT_DIR/CA
 cd $CERT_DIR/CA
 
 # Create the CA private key and certificate
-openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048 -subj "/CN=filebeat.lan/C=IL" -keyout filebeatCA.key -out filebeatCA.crt
+openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048 -subj "/CN=uc-ca/C=US" -keyout ucCA.key -out ucCA.crt
 
 # Create the demo directory
 mkdir -p $CERT_DIR/cert
@@ -28,13 +28,13 @@ mkdir -p $CERT_DIR/cert
 cd $CERT_DIR/cert
 
 # Create a private key for Filebeat
-openssl genrsa -out filebeat.key 2048
+openssl genrsa -out uc-key.pem 2048
 
 # Convert the private key to PKCS#8 format
-openssl pkcs8 -in filebeat.key -topk8 -out filebeat-pkcs8.key -nocrypt
+openssl pkcs8 -in uc-key.pem -topk8 -out uc-key-pkcs8.pem -nocrypt
 
 # Create a certificate request configuration file
-cat > filebeat-csr.conf <<EOF
+cat > uc-csr.conf <<EOF
 [ req ]
 default_bits = 2048
 prompt = no
@@ -43,8 +43,8 @@ req_extensions = req_ext
 distinguished_name = dn
 
 [ dn ]
-C = IL
-CN = filebeat.lan
+C = US
+CN = uc-filebeat
 
 [ req_ext ]
 subjectAltName = @alt_names
@@ -54,10 +54,10 @@ DNS.1 = $SERVER_DNS
 EOF
 
 # Create a certificate signing request for Filebeat
-openssl req -new -key filebeat.key -out filebeat.csr -config filebeat-csr.conf
+openssl req -new -key uc-key.pem -out uc-csr.pem -config uc-csr.conf
 
 # Create a certificate configuration file
-cat > filebeat-cert.conf <<EOF
+cat > uc-cert.conf <<EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -67,14 +67,14 @@ subjectAltName = @alt_names
 DNS.1 = $SERVER_DNS
 EOF
 
-# Create a certificate for Filebeat, signed by the CA
-openssl x509 -req -in filebeat.csr -CA ../CA/filebeatCA.crt -CAkey ../CA/filebeatCA.key -CAcreateserial -out filebeat.crt -days 365 -sha256 -extfile filebeat-cert.conf
+# Create a certificate for Universal Connector, signed by the CA
+openssl x509 -req -in uc-csr.pem -CA ../CA/ucCA.crt -CAkey ../CA/ucCA.key -CAcreateserial -out uc-cert.pem -days 365 -sha256 -extfile uc-cert.conf
 
-rm filebeat.csr
-rm filebeat-cert.conf
-rm filebeat-csr.conf
+rm uc-csr.pem
+rm uc-cert.conf
+rm uc-csr.conf
 
 cd ../CA
-rm filebeatCA.srl
+rm ucCA.srl
 
 echo "done"
