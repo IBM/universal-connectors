0601a129329845c4d000d21839001a87d17aba6e
ofer-haim <ofer.haim@ibm.com>
2024-08-28 13:28:25 +0300

    GRD-72478: added necessary permissions
    Signed-off-by: ofer-haim <ofer.haim@ibm.com>

--- build/prepareUCDefaultOfflinePackage.sh
+++ build/prepareUCDefaultOfflinePackage.sh
@@ -1,58 +1,70 @@
 #!/bin/bash
 
-LOGSTASH_PLUGIN_CMD=${LOGSTASH_DIR}/bin/logstash-plugin
+# Constants
+LOGSTASH_PLUGIN_CMD="${LOGSTASH_DIR}/bin/logstash-plugin"
+PACKAGES_LIST=""
 
-function addPluginToInstallationList() {
-  line=$1
-  if [[ $1 =~ ^#.* ]]; then
-    echo "Ignoring the commented out following line: $1"
+# Adds a plugin to the installation list if it is not commented out.
+add_plugin_to_installation_list() {
+  local line=$1
+
+  if [[ $line =~ ^# ]]; then
+    echo "Ignoring commented line: $line"
   else
-    pluginName=${1##*/}
-    packages_list+=" ${pluginName%-*}"
+    local plugin_name=${line##*/}
+    PACKAGES_LIST+=" ${plugin_name%-*}"
   fi
 }
 
-function removeUninstalledPluginFromPackagingList() {
-  installedPlugins=$(ls | grep gem)
-  for value in $packages_list
-  do
-    if [[ ${installedPlugins} != *"${value}"* ]];then
-      echo "${value} gem file was not found in the plugins directory. Removing it from default offline-package for UC"
-      packages_list=${packages_list//$value/}
+# Removes plugins from the packaging list that are not found in the directory.
+remove_uninstalled_plugins() {
+  local installed_plugins
+  installed_plugins=$(ls | grep -E '\.gem$')
+
+  for plugin in $PACKAGES_LIST; do
+    if [[ ! $installed_plugins == *"${plugin}"* ]]; then
+      echo "${plugin} gem file not found. Removing from packaging list."
+      PACKAGES_LIST=${PACKAGES_LIST// $plugin/}
     fi
   done
 }
 
-function removePluginsFromGIPackagingList() {
-  while read value; do
-    packages_list=${packages_list//$value/}
+# Removes plugins listed in build/pluginsToRemoveFromGIOfflinePackage.txt from the packaging list.
+remove_plugins_from_gi_list() {
+  while read -r plugin; do
+    PACKAGES_LIST=${PACKAGES_LIST// $plugin/}
   done < build/pluginsToRemoveFromGIOfflinePackage.txt
 }
 
-function prepareReleaseOfflinePackages() {
-  echo "Packages included in UC default offline package for GDP: $packages_list"
-  ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./uc_gdp_offline_package.zip --overwrite $packages_list
+# Prepares offline packages for UC and GI.
+prepare_offline_packages() {
+  echo "Creating UC default offline package for GDP with packages: $PACKAGES_LIST"
+  ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./uc_gdp_offline_package.zip --overwrite $PACKAGES_LIST
 
-  echo "Preparing separated offline packages for every built plugin..."
-  for value in $packages_list
-  do
-    ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./${value}.zip --overwrite ${value}
+  echo "Creating separated offline packages for each plugin..."
+  for plugin in $PACKAGES_LIST; do
+    ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./${plugin}.zip --overwrite ${plugin}
   done
 
-  removePluginsFromGIPackagingList
-  echo "Packages included in UC default offline package for GI: $packages_list"
-  ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./uc_gi_offline_package.zip --overwrite $packages_list
+  remove_plugins_from_gi_list
+  echo "Creating UC default offline package for GI with packages: $PACKAGES_LIST"
+  ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./uc_gi_offline_package.zip --overwrite $PACKAGES_LIST
 }
 
-while read line; do addPluginToInstallationList $line; done <build/defaultOfflinePackagePlugins.txt
+# Main script execution
+# Build the list of plugins to install
+while read -r line; do
+  add_plugin_to_installation_list "$line"
+done < build/defaultOfflinePackagePlugins.txt
 
 echo "Installing packages on Logstash..."
-cd ${LOGSTASH_DIR}
+cd "${LOGSTASH_DIR}" || exit
 ${LOGSTASH_PLUGIN_CMD} install *.gem
 
-removeUninstalledPluginFromPackagingList
+remove_uninstalled_plugins
+prepare_offline_packages
 
-prepareReleaseOfflinePackages
-
+# Move zip files to the dist directory
 mkdir -p universal-connectors/dist
 mv *.zip universal-connectors/dist/
+
