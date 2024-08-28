e3a2f681e6c69336fca708f285d11c4b205c579e
ofer-haim <ofer.haim@ibm.com>
2024-08-28 11:54:26 +0300

    GRD-72478: first sketch of the build script
    Signed-off-by: ofer-haim <ofer.haim@ibm.com>

---
+++ build/buildUCPluginGems.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+BASE_DIR=$(pwd)
+
+function adjustToLogstash8() {
+  IS_MAC_M1_BUILD=true # change to true to build on Mac M1 build
+  if [ "$IS_MAC_M1_BUILD" = true ]; then
+    sed 's/logstash-core-.*\.jar/logstash-core.jar/' build.gradle > tmp && mv tmp build.gradle
+    #sed '/ext { snakeYamlVersion.*/d' build.gradle > tmp && mv tmp build.gradle
+    #sed  "/^buildscript.*/a ext { snakeYamlVersion = \"$snakeYamlVersion\" }" build.gradle > tmp && mv tmp build.gradle
+  else
+    sed -i 's/logstash-core-*.*.*.jar/logstash-core.jar/' build.gradle
+    #sed -i '/ext { snakeYamlVersion.*/d' build.gradle
+    #sed -i '/^buildscript.*/a ext { snakeYamlVersion = '$snakeYamlVersion' }' build.gradle
+  fi
+}
+function buildUCPluginGem() {
+  echo "================ Building $1 gem file================"
+  cd ${BASE_DIR}/$1
+  adjustToLogstash8
+  cp ${BASE_DIR}/build/gradle.properties .
+  chmod 755 gradlew
+  ./gradlew --no-daemon test </dev/null >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Successfully test $1"
+      ./gradlew --no-daemon gem </dev/null >/dev/null 2>&1
+      if [ $? -eq 0 ]; then
+        echo "Successfully build gem $1"
+      else
+        echo "Failed build gem $1"
+      fi
+  else
+    echo "Failed test $1"
+  fi
+}
+
+function buildUCCommons() {
+  cd ${BASE_DIR}/common
+  ./gradlew test >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Successfully test uc-commons"
+  else
+    echo "Failed test uc-commons"
+    exit 1
+  fi
+  #check if succeed
+  ./gradlew jar >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Successfully build jar uc-commons"
+  else
+    echo "Failed build jar uc-commons"
+    exit 2
+  fi
+  cp ./build/libs/common-1.0.0.jar ./build/libs/guardium-universalconnector-commons-1.0.0.jar
+  cd ../../
+}
+
+function buildRubyPlugin(){
+  cd ${BASE_DIR}/$1
+  bundle install >/dev/null 2>&1
+  gem build $2
+}
+
+buildUCCommons
+
+#snakeYamlVersion=$(grep -E "snakeYamlVersion =" /usr/share/logstashSRC/logstash/build.gradle | cut -d \' -f 2)
+
+# Build the rest of the plugins from javaPluginsToBuild.txt
+grep -v '^#' ${BASE_DIR}/build/javaPluginsToBuild.txt | while read -r line; do buildUCPluginGem "$line";done
+grep -v '^#' ${BASE_DIR}/build/rubyPluginsToBuild.txt | while read -r line; do buildRubyPlugin "${line}" "${line##*/}.gemspec"; done
+
+exit 0
---
+++ build/prepareUCDefaultOfflinePackage.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+LOGSTASH_PLUGIN_CMD=/usr/share/logstash/bin/logstash-plugin
+
+function addPluginToInstallationList() {
+  line=$1
+  if [[ $1 =~ ^#.* ]]; then
+    echo "Ignoring the commented out following line: $1"
+  else
+    pluginName=${1##*/}
+    packages_list+=" ${pluginName%-*}"
+  fi
+}
+
+function removeUninstalledPluginFromPackagingList() {
+  installedPlugins=$(ls | grep gem)
+  for value in $packages_list
+  do
+    if [[ ${installedPlugins} != *"${value}"* ]];then
+      echo "${value} gem file was not found in the plugins directory. Removing it from default offline-package for UC"
+      packages_list=${packages_list//$value/}
+    fi
+  done
+}
+
+function removePluginsFromGIPackagingList() {
+  while read value; do
+    packages_list=${packages_list//$value/}
+  done < build/pluginsToRemoveFromGIOfflinePackage.txt
+}
+
+function prepareReleaseOfflinePackages() {
+  echo "Packages included in UC default offline package for GDP: $packages_list"
+  ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./uc_gdp_offline_package.zip --overwrite $packages_list
+
+  echo "Preparing separated offline packages for every built plugin..."
+  for value in $packages_list
+  do
+    ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./${value}.zip --overwrite ${value}
+  done
+
+  removePluginsFromGIPackagingList
+  echo "Packages included in UC default offline package for GI: $packages_list"
+  ${LOGSTASH_PLUGIN_CMD} prepare-offline-pack --output ./uc_gi_offline_package.zip --overwrite $packages_list
+}
+
+while read line; do addPluginToInstallationList $line; done <build/defaultOfflinePackagePlugins.txt
+
+echo "Installing packages on Logstash..."
+${LOGSTASH_PLUGIN_CMD} install *.gem
+
+removeUninstalledPluginFromPackagingList
+
+prepareReleaseOfflinePackages
+
+mkdir -p dist
+mv *.zip dist/
