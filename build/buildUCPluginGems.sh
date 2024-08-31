b4cef798550199340aef8055898b498b6368a0e9
ofer-haim <ofer.haim@ibm.com>
2024-08-31 08:25:38 +0300

    GRD-72478: test- GNU parallel
    Signed-off-by: ofer-haim <ofer.haim@ibm.com>

--- build/buildUCPluginGems.sh
+++ build/buildUCPluginGems.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 BASE_DIR=$(pwd)
 
+function buildUCCommons() {
+  cd "${BASE_DIR}/common" || exit
+  ./gradlew test >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Successfully tested uc-commons"
+  else
+    echo "Failed to test uc-commons"
+    exit 1
+  fi
+  ./gradlew jar >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    echo "Successfully built jar uc-commons"
+  else
+    echo "Failed to build jar uc-commons"
+    exit 2
+  fi
+  cp ./build/libs/common-1.0.0.jar ./build/libs/guardium-universalconnector-commons-1.0.0.jar
+  cd "${BASE_DIR}" || exit
+}
+
+buildUCCommons
+
+# Build Java plugins in parallel
+grep -v '^#' "${BASE_DIR}/build/javaPluginsToBuild.txt" | xargs -P 4 -I {} bash -c '
+BASE_DIR="'${BASE_DIR}'"
+
 function adjustToLogstash8() {
-  sed -i 's/logstash-core-*.*.*.jar/logstash-core.jar/' build.gradle
+  sed -i "s/logstash-core-*.*.*.jar/logstash-core.jar/" build.gradle
 }
 
 function buildUCPluginGem() {
@@ -24,25 +50,12 @@ function buildUCPluginGem() {
   fi
 }
 
-function buildUCCommons() {
-  cd "${BASE_DIR}/common" || exit
-  ./gradlew test >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    echo "Successfully tested uc-commons"
-  else
-    echo "Failed to test uc-commons"
-    exit 1
-  fi
-  ./gradlew jar >/dev/null 2>&1
-  if [ $? -eq 0 ]; then
-    echo "Successfully built jar uc-commons"
-  else
-    echo "Failed to build jar uc-commons"
-    exit 2
-  fi
-  cp ./build/libs/common-1.0.0.jar ./build/libs/guardium-universalconnector-commons-1.0.0.jar
-  cd "${BASE_DIR}" || exit
-}
+buildUCPluginGem "{}"
+'
+
+# Build Ruby plugins in parallel
+grep -v '^#' "${BASE_DIR}/build/rubyPluginsToBuild.txt" | xargs -P 2 -I {} bash -c '
+BASE_DIR="'${BASE_DIR}'"
 
 function buildRubyPlugin() {
   cd "${BASE_DIR}/$1" || exit
@@ -50,12 +63,7 @@ function buildRubyPlugin() {
   gem build "$2"
 }
 
-buildUCCommons
-
-# Build Java plugins in parallel
-grep -v '^#' "${BASE_DIR}/build/javaPluginsToBuild.txt" | xargs -P 4 -I {} bash -c 'bash -c "source ${BASE_DIR}/build/buildUCPluginGem.sh; buildUCPluginGem \"{}\""'
-
-# Build Ruby plugins in parallel
-grep -v '^#' "${BASE_DIR}/build/rubyPluginsToBuild.txt" | xargs -P 2 -I {} bash -c 'bash -c "source ${BASE_DIR}/build/buildRubyPlugin.sh; buildRubyPlugin \"{}\" \"${}/*.gemspec\""'
+buildRubyPlugin "{}" "${}/*.gemspec"
+'
 
 exit 0
