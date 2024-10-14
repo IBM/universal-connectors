8dff7e4cb45c8d580286033a53e858c722ce18bb
Chirag Soni <chirag.soni@ibm.com>
2024-10-14 19:52:28 +0530

    fix: INS-48042 pubsub apache solar plugin upgrade to java 17
    Signed-off-by: Chirag Soni <chirag.soni@ibm.com>

--- filter-plugin/logstash-filter-pubsub-apachesolr-guardium/src/test/java/com/ibm/guardium/apachesolrdb/ParserTest.java
+++ filter-plugin/logstash-filter-pubsub-apachesolr-guardium/src/test/java/com/ibm/guardium/apachesolrdb/ParserTest.java
@@ -1,5 +1,5 @@
 /*
-© Copyright IBM Corp. 2021, 2022 All rights reserved.
+ï¿½ Copyright IBM Corp. 2021, 2022 All rights reserved.
 SPDX-License-Identifier: Apache-2.0
 */
 package com.ibm.guardium.apachesolrdb;
@@ -13,7 +13,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ibm.guardium.universalconnector.commons.structures.Accessor;
 import com.ibm.guardium.universalconnector.commons.structures.ExceptionRecord;
-import com.ibm.guardium.universalconnector.commons.structures.Record;
+import com.ibm.guardium.universalconnector.commons.structures.UCRecord;
 import com.ibm.guardium.universalconnector.commons.structures.Sentence;
 import com.ibm.guardium.universalconnector.commons.structures.SessionLocator;
 
@@ -32,7 +32,7 @@ public class ParserTest {
 	public void testParseQtpConstruct_update() throws Exception {
 		final String solrString = "{\"insertId\":\"epzzqmg1c6000v\",\"jsonPayload\":{\"message\":\"2022-02-10 12:42:05.050 INFO  (qtp672313607-16) [   x:first_core] o.a.s.u.p.LogUpdateProcessorFactory [first_core]  webapp=\\/solr path=\\/update params={commitWithin=1000&overwrite=true&wt=json&_=1644496890256}{add=[7698 (1724380007675985920)]} 0 6\"},\"resource\":{\"type\":\"gce_instance\",\"labels\":{\"project_id\":\"project-sccd\",\"zone\":\"asia-south2-a\",\"instance_id\":\"2639899388249855782\"}},\"timestamp\":\"2022-02-10T12:42:05.050746475Z\",\"logName\":\"projects\\/project-sccd\\/logs\\/files\",\"receiveTimestamp\":\"2022-02-10T12:42:05.438339552Z\"}";
 		final JsonObject solrJson = JsonParser.parseString(solrString).getAsJsonObject();
-		Record record = Parser.parseQtpRecord(solrJson);
+		UCRecord record = Parser.parseQtpRecord(solrJson);
 		Sentence sentence = record.getData().getConstruct().getSentences().get(0);
 		Assert.assertNotNull(sentence);
 		Assert.assertNotNull(record);
@@ -45,7 +45,7 @@ public class ParserTest {
 	public void testParseQtpConstruct_Find() throws Exception {
 		final String solrString = "{\"insertId\":\"epzzqmg1c6dwlg\",\"jsonPayload\":{\"message\":\"2022-02-10 12:44:20.243 INFO  (qtp672313607-18) [   x:first_core] o.a.s.c.S.Request [first_core]  webapp=\\/solr path=\\/select params={q=id:7698&_=1644497065596} hits=1 status=0 QTime=1\"},\"resource\":{\"type\":\"gce_instance\",\"labels\":{\"project_id\":\"project-sccd\",\"instance_id\":\"2639899388249855782\",\"zone\":\"asia-south2-a\"}},\"timestamp\":\"2022-02-10T12:44:20.244030006Z\",\"logName\":\"projects\\/project-sccd\\/logs\\/files\",\"receiveTimestamp\":\"2022-02-10T12:44:20.439608402Z\"}";
 		final JsonObject solrJson = JsonParser.parseString(solrString).getAsJsonObject();
-		Record record = Parser.parseQtpRecord(solrJson);
+		UCRecord record = Parser.parseQtpRecord(solrJson);
 		final Sentence sentence = record.getData().getConstruct().getSentences().get(0);
 		Assert.assertNotNull(sentence);
 		Assert.assertEquals("select", sentence.getVerb());
@@ -57,7 +57,7 @@ public class ParserTest {
 	public void testParseAccessor() throws Exception {
 		final String solrString = "{\"insertId\":\"epzzqmg1c6dwlg\",\"jsonPayload\":{\"message\":\"2022-02-10 12:44:20.243 INFO  (qtp672313607-18) [   x:first_core] o.a.s.c.S.Request [first_core]  webapp=\\/solr path=\\/select params={q=id:7698&_=1644497065596} hits=1 status=0 QTime=1\"},\"resource\":{\"type\":\"gce_instance\",\"labels\":{\"project_id\":\"project-sccd\",\"instance_id\":\"2639899388249855782\",\"zone\":\"asia-south2-a\"}},\"timestamp\":\"2022-02-10T12:44:20.244030006Z\",\"logName\":\"projects\\/project-sccd\\/logs\\/files\",\"receiveTimestamp\":\"2022-02-10T12:44:20.439608402Z\"}";
 		final JsonObject solrJson = JsonParser.parseString(solrString).getAsJsonObject();
-		Record record = Parser.parseQtpRecord(solrJson);
+		UCRecord record = Parser.parseQtpRecord(solrJson);
 		final Accessor accessor = record.getAccessor();
 		Assert.assertNotNull(accessor);
 		Assert.assertEquals("ApacheSolrGCP", accessor.getDbProtocol().toString());
@@ -71,7 +71,7 @@ public class ParserTest {
 	public void testParseSessionLocator() throws Exception {
 		final String solrString = "{\"insertId\":\"epzzqmg1c6dwlg\",\"jsonPayload\":{\"message\":\"2022-02-10 12:44:20.243 INFO  (qtp672313607-18) [   x:first_core] o.a.s.c.S.Request [first_core]  webapp=\\/solr path=\\/select params={q=id:7698&_=1644497065596} hits=1 status=0 QTime=1\"},\"resource\":{\"type\":\"gce_instance\",\"labels\":{\"project_id\":\"project-sccd\",\"instance_id\":\"2639899388249855782\",\"zone\":\"asia-south2-a\"}},\"timestamp\":\"2022-02-10T12:44:20.244030006Z\",\"logName\":\"projects\\/project-sccd\\/logs\\/files\",\"receiveTimestamp\":\"2022-02-10T12:44:20.439608402Z\"}";
 		final JsonObject solrJson = JsonParser.parseString(solrString).getAsJsonObject();
-		Record record = Parser.parseQtpRecord(solrJson);
+		UCRecord record = Parser.parseQtpRecord(solrJson);
 		final SessionLocator sessionLocator = record.getSessionLocator();
 		Assert.assertNotNull(sessionLocator);
 		Assert.assertEquals("0.0.0.0", sessionLocator.getClientIp().toString());
@@ -84,7 +84,7 @@ public class ParserTest {
 	public void testParseException() throws Exception {
 		final String solrString = "{\"insertId\":\"17u90yhf9nmdte\",\"jsonPayload\":{\"message\":\"2022-02-10 14:04:38.542 ERROR  (qtp672313607-17) [   x:first_core] o.a.s.h.RequestHandlerBase org.apache.solr.common.SolrException: Cannot parse provided JSON: Unexpected EOF: char=(EOF),position=20 AFTER=''\"},\"resource\":{\"type\":\"gce_instance\",\"labels\":{\"instance_id\":\"2639899388249855782\",\"zone\":\"asia-south2-a\",\"project_id\":\"project-sccd\"}},\"timestamp\":\"2022-02-10T14:04:38.543314031Z\",\"logName\":\"projects\\/project-sccd\\/logs\\/files\",\"receiveTimestamp\":\"2022-02-10T14:04:39.447195015Z\"}";
 		final JsonObject solrJson = JsonParser.parseString(solrString).getAsJsonObject();
-		Record record = Parser.parseQtpRecord(solrJson);
+		UCRecord record = Parser.parseQtpRecord(solrJson);
 		final ExceptionRecord exceptionRecord = record.getException();
 		Assert.assertNotNull(exceptionRecord);
 		Assert.assertEquals(ApplicationConstant.EXCEPTION_TYPE_AUTHORIZATION_STRING,
@@ -98,7 +98,7 @@ public class ParserTest {
 	public void testRedacted() throws Exception {
 		final String solrString = "{\"insertId\":\"1lzqgspg20hyho1\",\"jsonPayload\":{\"message\":\"2022-06-03 11:27:38.004 INFO  (qtp2005169944-19) [   ] o.a.s.s.HttpSolrCall [admin] webapp=null path=\\/admin\\/cores params={core=firstCore&other=secondCore&action=SWAP} status=0 QTime=207\"},\"resource\":{\"type\":\"gce_instance\",\"labels\":{\"zone\":\"asia-south2-a\",\"instance_id\":\"6290282768426383618\",\"project_id\":\"project-sccd\"}},\"timestamp\":\"2022-05-30T08:50:36.941752806Z\",\"labels\":{\"compute.googleapis.com\\/resource_name\":\"apachesolrqa\"},\"logName\":\"projects\\/project-sccd\\/logs\\/files\",\"receiveTimestamp\":\"2022-05-30T08:50:37.641620487Z\"}";
 		final JsonObject solrJson = JsonParser.parseString(solrString).getAsJsonObject();
-		Record record = Parser.parseQtpRecord(solrJson);
+		UCRecord record = Parser.parseQtpRecord(solrJson);
 		String redacted = record.getData().getConstruct().getRedactedSensitiveDataSql();
 		Assert.assertNotNull(redacted);
 		Assert.assertNotNull(record);
