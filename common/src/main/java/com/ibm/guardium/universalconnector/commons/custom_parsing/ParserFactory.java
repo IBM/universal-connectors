2d8925efe3c9aea221f1b675e2dbe30c30ffae3d
Taees Eimouri <taees.eimouri1@ibm.com>
2025-02-07 13:23:22 -0500

    INS-49047: Adding JSON/Leef parser
    [UC Editor] adding JSON/Leef custom parser
    
    Signed-off-by: Taees Eimouri <taees.eimouri1@ibm.com>
    Signed-off-by: Zeeshan Ali <zeeshan.ali@ibm.com>

--- common/src/main/java/com/ibm/guardium/universalconnector/commons/custom_parsing/ParserFactory.java
+++ common/src/main/java/com/ibm/guardium/universalconnector/commons/custom_parsing/ParserFactory.java
@@ -1,23 +1,26 @@
 package com.ibm.guardium.universalconnector.commons.custom_parsing;
 
 import com.ibm.guardium.universalconnector.commons.custom_parsing.parsers.IParser;
+import com.ibm.guardium.universalconnector.commons.custom_parsing.parsers.JsonParser;
+import com.ibm.guardium.universalconnector.commons.custom_parsing.parsers.LeefParser;
 import com.ibm.guardium.universalconnector.commons.custom_parsing.parsers.RegexParser;
 
 public class ParserFactory {
-    IParser parser;
-
     public ParserFactory() {
     }
 
     public IParser getParser(ParserType parserType) {
-        //For now we only support Regex
-        //if(parserType.equals(ParserType.regex))
+        if (parserType.equals(ParserType.json))
+            return new JsonParser();
+        else if (parserType.equals(ParserType.leef))
+            return new LeefParser();
+
         return new RegexParser();
     }
 
     public enum ParserType {
         regex,
         json,
-        xml
+        leef
     }
 }
---
+++ common/src/main/java/com/ibm/guardium/universalconnector/commons/custom_parsing/excepton/InvalidConfigurationException.java
@@ -0,0 +1,7 @@
+package com.ibm.guardium.universalconnector.commons.custom_parsing.excepton;
+
+public class InvalidConfigurationException extends Exception {
+    public InvalidConfigurationException(String msg) {
+        super(msg);
+    }
+}
