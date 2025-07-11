ab6cf5a506c977ae95748d7df7c051038d2b4d56
Jessica Du <jingqiu@ca.ibm.com>
2025-07-11 12:04:20 -0400

    Merge Capella filter plugin into public repo
    Signed-off-by: Jessica Du <jingqiu@ca.ibm.com>

---
+++ filter-plugin/logstash-filter-capella-guardium/src/main/java/com/ibm/guardium/capella/Constants.java
@@ -0,0 +1,22 @@
+package com.ibm.guardium.capella;
+
+public class Constants {
+  static final String MESSAGE = "message";
+  static final String INVALID_MSG = "EVENT_IS_INVALID";
+  static final String NOT_AVAILABLE = "N.A.";
+  static final String BUCKET = "bucket";
+  static final String OBJECT = "object";
+  static final String INDEX = "index";
+  static final String VERB = "verb";
+  static final String TIMESTAMP = "timestamp";
+  static final String DB_USER_DOMAIN = "db_user_domain";
+  static final String DB_USER = "db_user";
+  static final String STATEMENT = "statement";
+  static final String DB_NAME = "db_name";
+  static final String COUCHBASE = "COUCHB";
+  public static final String PREPENDED_STRING = "__CB POST /#statement=";
+  static final String LOGIN_FAILED = "LOGIN_FAILED";
+  static final String SQL_ERROR = "SQL_ERROR";
+  static final String SQL_STATUS = "status";
+  static final String SUCCESS_STATUS = "success";
+}
