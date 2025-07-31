da5123ae127c672b592ffbc2642fa329f77408d7
Glenn <glenn.lee@ibmc.om>
2025-07-31 11:13:53 -0400

    GRD-87207 - Fixed IndexOutOfBoundsException seen in uc-logs for Redshift
    Signed-off-by: Glenn <glenn.lee@ibmc.om>
    Signed-off-by: Huiting Hu <laurel.h@ibm.com>

--- filter-plugin/logstash-filter-redshift-aws-guardium/src/test/java/com/ibm/guardium/redshift/ParserTest.java
+++ filter-plugin/logstash-filter-redshift-aws-guardium/src/test/java/com/ibm/guardium/redshift/ParserTest.java
@@ -17,6 +17,7 @@ import com.ibm.guardium.universalconnector.commons.structures.Time;
 import co.elastic.logstash.api.Event;
 
 class ParserTest {
+	Parser parser = new Parser();
 	@Test
 	public void testparseAccessor() {
 		Record record = new Record();
@@ -24,7 +25,7 @@ class ParserTest {
 		Event e = new org.logstash.Event();
 		e.setField("username", "awsuser");
 		e.setField("os_version", "Linux 4.14.262-200.489.amzn2.x86_64 amd64");
-		final Accessor accessor = Parser.parseAccessor(e, record);
+		final Accessor accessor = parser.parseAccessor(e, record);
 		assertNotNull(accessor);
 		assertEquals("awsuser", accessor.getDbUser());
 		assertEquals("Linux 4.14.262-200.489.amzn2.x86_64 amd64", accessor.getOsUser());
@@ -35,7 +36,7 @@ class ParserTest {
 		Event e = new org.logstash.Event();
 		e.setField(RedShiftTags.REMOTEHOST, "223.233.72.13");
 		e.setField(RedShiftTags.REMOTEPORT, "31370");
-		final SessionLocator sessionLocator = Parser.parseSessionLocator(e);
+		final SessionLocator sessionLocator = parser.parseSessionLocator(e);
 		assertNotNull(sessionLocator);
 		assertEquals("223.233.72.13", sessionLocator.getClientIp());
 		assertEquals(31370, sessionLocator.getClientPort());
@@ -46,7 +47,7 @@ class ParserTest {
 		Event e = new org.logstash.Event();
 		e.setField("remotehost", "::1");
 		e.setField("remoteport", "31370");
-		final SessionLocator sessionLocator = Parser.parseSessionLocator(e);
+		final SessionLocator sessionLocator = parser.parseSessionLocator(e);
 		assertNotNull(sessionLocator);
 		assertEquals("::1", sessionLocator.getClientIpv6());
 		assertEquals(31370, sessionLocator.getClientPort());
@@ -56,7 +57,7 @@ class ParserTest {
 	public void testParseData() {
 		Event e = new org.logstash.Event();
 		e.setField(RedShiftTags.SQLQUERY, "SELECT d.datname as Name");
-		final Data data = Parser.parseData(e);
+		final Data data = parser.parseData(e);
 		assertNotNull(data);
 		assertEquals("SELECT d.datname as Name", data.getOriginalSqlCommand());
 	}
@@ -69,16 +70,16 @@ class ParserTest {
 		e.setField("md", "16");
 		e.setField("year", "2022");
 		e.setField("time", "04:58:51:641");
-		final Time contime = Parser.getConnTime(e);
+		final Time contime = parser.getConnTime(e);
 		assertNotNull(contime);
-		assertEquals(1647386931641L, contime.getTimstamp());
+		assertEquals(1647421131641L, contime.getTimstamp());
 	}
 
 	@Test
 	public void testparsegetTime() {
 		Event e = new org.logstash.Event();
 		e.setField("timestamp", "2022-03-17T07:34:29.118Z");
-		final Time time = Parser.getTime(e);
+		final Time time = parser.getTime(e);
 		assertNotNull(time);
 		assertEquals(1647502469118L, time.getTimstamp());
 	}
@@ -87,7 +88,7 @@ class ParserTest {
 	public void testparseException() {
 		Event e = new org.logstash.Event();
 		e.setField("action", "authentication failure");
-		final ExceptionRecord exceptionRecord = Parser.parseException(e);
+		final ExceptionRecord exceptionRecord = parser.parseException(e);
 		assertNotNull(exceptionRecord);
 		assertEquals("LOGIN_FAILED", exceptionRecord.getExceptionTypeId());
 		assertEquals("LOGIN_FAILED", exceptionRecord.getDescription());
@@ -107,12 +108,12 @@ class ParserTest {
 		e.setField("year", "2022");
 		e.setField("time", "04:58:51:641");
 		e.setField("action", "authentication failure");
-		final Record record = Parser.parseRecord(e);
+		final Record record = parser.parseRecord(e);
 		assertNotNull(record);
 		assertEquals("796", record.getSessionId());
 		assertEquals("979326520502_guardiumredshift:dev", record.getDbName());
 		assertEquals("awsuser", record.getAppUserName());
-		assertEquals(1647386931641L, record.getTime().getTimstamp());
+		assertEquals(1647421131641L, record.getTime().getTimstamp());
 		assertEquals("LOGIN_FAILED", record.getException().getExceptionTypeId());
 		assertEquals("LOGIN_FAILED", record.getException().getDescription());
 
@@ -127,7 +128,7 @@ class ParserTest {
 		e.setField("user", "awsuser");
 		e.setField("timestamp", "2022-03-17T07:34:29.118Z");
 		e.setField("sql_query", "SELECT d.datname as Name");
-		final Record record = Parser.parseRecord(e);
+		final Record record = parser.parseRecord(e);
 		assertNotNull(record);
 		assertEquals("1073979586", record.getSessionId());
 		assertEquals("979326520502_guardiumredshift:dev", record.getDbName());
@@ -142,7 +143,7 @@ class ParserTest {
 		e.setField("pid", "1073979586");
 		e.setField("timestamp", "2022-03-17T07:34:29.118Z");
 		e.setField("sql_query", "SELECT d.datname as Name");
-		final Record record = Parser.parseRecord(e);
+		final Record record = parser.parseRecord(e);
 		assertNotNull(record);
 		assertEquals("1073979586", record.getSessionId());
 		assertEquals(1647502469118L, record.getTime().getTimstamp());
@@ -154,7 +155,7 @@ class ParserTest {
 		Event e = new org.logstash.Event();
 		e.setField("remotehost", "::ffff:136.226.255.29");
 		e.setField("remoteport", "31370");
-		final SessionLocator sessionLocator = Parser.parseSessionLocator(e);
+		final SessionLocator sessionLocator = parser.parseSessionLocator(e);
 		assertNotNull(sessionLocator);
 		assertEquals("::ffff:136.226.255.29", sessionLocator.getClientIpv6());
 		assertEquals(31370, sessionLocator.getClientPort());
