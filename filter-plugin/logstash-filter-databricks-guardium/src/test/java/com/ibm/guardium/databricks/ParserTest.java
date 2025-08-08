2cc9ee47398684a8b2ac79673343958e682dafd4
Huiting Hu <laurel.h@ibm.com>
2025-08-08 17:36:10 -0400

    aug 8
    Signed-off-by: Huiting Hu <laurel.h@ibm.com>

--- filter-plugin/logstash-filter-databricks-guardium/src/test/java/com/ibm/guardium/databricks/ParserTest.java
+++ filter-plugin/logstash-filter-databricks-guardium/src/test/java/com/ibm/guardium/databricks/ParserTest.java
@@ -23,7 +23,7 @@ class ParserTest {
         final String DatabricksString = "{ \"resourceId\": \"/SUBSCRIPTIONS/5C0C81D4-656F-415D-8599-DCD86F2F665E/RESOURCEGROUPS/DATABRICKSTEST/PROVIDERS/MICROSOFT.DATABRICKS/WORKSPACES/DATABRICK-TEST\", \"operationVersion\": \"1.0.0\", \"identity\": \"{\\\"email\\\":\\\"abc.h@abc.com\\\",\\\"subjectName\\\":null}\", \"operationName\": \"Microsoft.Databricks/accounts/tokenLogin\", \"time\": \"2025-05-01T13:43:28Z\", \"category\": \"accounts\", \"properties\": {\"sourceIPAddress\":\"20.193.136.102\",\"logId\":\"87e1a69e-444a-434e-a0be-648b1797e6d9\",\"serviceName\":\"accounts\",\"userAgent\":\"Apache-HttpClient/4.5.14 (Java/17.0.13) Databricks-Service/driver DBHttpClient/v2RawClient\",\"response\":\"{\\\"statusCode\\\":200}\",\"sessionId\":null,\"actionName\":\"tokenLogin\",\"requestId\":\"0018d88c-2f52-4cf0-86a4-8d1dc416ab10\",\"requestParams\":\"{\\\"user\\\":\\\"abc@abc.com\\\",\\\"tokenId\\\":\\\"kfhjgjfhdgjkdh39284783297423943hejhfkdsfh39\\\",\\\"authenticationMethod\\\":\\\"API_INT_PAT_TOKEN\\\"}\"}, \"Host\": \"1234-123456-ab1c2d3e-12-123-12-1\"}";
         final JsonObject DatabricksJson = JsonParser.parseString(DatabricksString).getAsJsonObject();
         Record record = Parser.parseRecord(DatabricksJson);
-        assertEquals("accounts", record.getAccessor().getServiceName());
+        assertEquals("N.A.", record.getAccessor().getServiceName());
 
     }
 
