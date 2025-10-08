b09b4984d02e4c87a474c62e50cfefa2d1838f34
Zeeshan Ali <zeeshan.ali@ibm.com>
2025-10-08 13:05:23 -0400

    GRD-111655 Port Milvus UC source code to public repo (#969)
    * GRD-111655 Port Milvus UC source code to public repo
    
    * GRD-111655 remove zip file from public repo
    
    Signed-off-by: Andy.Chen <andy.chen@ibm.com>

---
+++ filter-plugin/logstash-filter-milvus-guardium/src/main/java/com/ibm/guardium/milvus/Constants.java
@@ -0,0 +1,17 @@
+package com.ibm.guardium.milvus;
+
+public class Constants {
+    public static final String DATA_TYPE = "TEXT";
+    public static final String LANGUAGE = "Milvus";
+    static final String MESSAGE = "message";
+    static final String INVALID_MSG = "EVENT_IS_INVALID";
+    static final String EMPTY = "";
+    static final String SQL_ERROR = "SQL_ERROR";
+    static final String LOGIN_FAILED = "LOGIN_FAILED";
+    static final String ERROR_MSG = "exception_desc";
+    static final String EVENT_ID = "event_id";
+    static final String USER_ADDR = "client";
+    static final String DB_PROTOCOL = "MILVUS GRPC";
+    static final String METHOD_STATUS = "method_status";
+
+}
