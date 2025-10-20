317dbc8906a64dbd7758aeb3233f8549247a4889
laurel-hu <laurel.h@ibm.com>
2025-10-20 11:22:53 -0400

    Oct 20
    Signed-off-by: laurel-hu <laurel.h@ibm.com>
    Signed-off-by: Gerth, Matthias <Matthias.Gerth@softwareag.com>

--- filter-plugin/logstash-filter-teradatadb-guardium/src/main/java/com/ibm/guardium/teradatadb/Constants.java
+++ filter-plugin/logstash-filter-teradatadb-guardium/src/main/java/com/ibm/guardium/teradatadb/Constants.java
@@ -6,6 +6,7 @@ public interface Constants {
 	public static final String DEFAULT_IP = "0.0.0.0";
 	public static final int DEFAULT_PORT = -1;
 	public static final String UNKNOWN_STRING = "";
+	public static final String NOT_AVAILABLE = "N.A.";
 	public static final String SERVER_TYPE_STRING = "TRD";
 	public static final String DATA_PROTOCOL_STRING = "TeradataDB native audit";
 	public static final String TERADATA_LANGUAGE="TRD";
