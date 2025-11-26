ada2d8ce558a461aeac32f664eee5b868f148fad
nida.bandukwala <nida.bandukwala@ibm.com>
2025-11-26 17:41:06 -0500

    Updating parser
    Signed-off-by: nida.bandukwala <nida.bandukwala@ibm.com>

--- filter-plugin/logstash-filter-azure-sql-guardium/src/main/java/com/ibm/guardium/azureSQL/Parser.java
+++ filter-plugin/logstash-filter-azure-sql-guardium/src/main/java/com/ibm/guardium/azureSQL/Parser.java
@@ -10,96 +10,109 @@ import com.ibm.guardium.universalconnector.commons.structures.ExceptionRecord;
 import com.ibm.guardium.universalconnector.commons.structures.Record;
 import com.ibm.guardium.universalconnector.commons.structures.SessionLocator;
 import com.ibm.guardium.universalconnector.commons.structures.Time;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import co.elastic.logstash.api.Event;
 
 public class Parser {
-	
-	public static Record parseRecord(final Event e) throws ParseException {
-		
-			Record record = new Record();
-		
-			record.setSessionId(e.getField(Constants.Session_ID).toString());
 
-			String databaseName = Constants.UNKNOWN_STRING;
-			if(e.getField(Constants.DATABASE_NAME) !=null){
-				databaseName=e.getField(Constants.DATABASE_NAME).toString();
-			}
-			record.setDbName(databaseName);
-			
-			record.setAppUserName(Constants.APP_USER_NAME);
-			
-			record.setTime(Parser.parseTimestamp(e));
-			
-			record.setSessionLocator(Parser.parseSessionLocator(e));
-			
-			record.setAccessor(Parser.parseAccessor(e));
-			
-				if(e.getField(Constants.SUCCEEDED).toString().contains("true")){
-					Data data=new Data();
-					data.setOriginalSqlCommand(e.getField(Constants.STATEMENT).toString());
-					record.setData(data);
-				}else {
-					ExceptionRecord exceptionRecord = new ExceptionRecord();
-					exceptionRecord.setExceptionTypeId(Constants.SQL_ERROR);
-					exceptionRecord.setDescription(e.getField(Constants.ADDITIONAL_INFORMATION).toString());
-					exceptionRecord.setSqlString(e.getField(Constants.STATEMENT).toString());
-					record.setException(exceptionRecord);
-				}
-		return record;
-	}
-	
-	public static Time parseTimestamp(final Event e) {
-		
-		long date=0;
-			String dateString = e.getField(Constants.TIMESTAMP).toString();
-			date=Long.parseLong(dateString);
-			long mini=date/1000000;
-			
-			return new Time(mini,0,0);
-	}
+  private static String getFieldString(final Event e, final String key, final String defaulString) {
+    Object v = e.getField(key);
+    if (v == null) {
+      return defaulString;
+    }
+    return v.toString();
+  }
 
-	public static SessionLocator parseSessionLocator(final Event e) {
+  public static Record parseRecord(final Event e) throws ParseException {
 
-				SessionLocator sessionLocator = new SessionLocator();
-			
-				sessionLocator.setClientIp(e.getField(Constants.Client_IP).toString());
-				sessionLocator.setClientPort(Constants.DEFAULT_PORT);
-				sessionLocator.setServerIp(Constants.DEFAULT_IP);
-				sessionLocator.setServerPort(Constants.DEFAULT_PORT);
-				sessionLocator.setIpv6(false);
-				sessionLocator.setClientIpv6(Constants.UNKNOWN_STRING);
-				sessionLocator.setServerIpv6(Constants.UNKNOWN_STRING);
+    Record record = new Record();
+    record.setSessionId(getFieldString(e, Constants.Session_ID, Constants.UNKNOWN_STRING));
 
-			return sessionLocator;
-	}
-	
-	public static Accessor parseAccessor(final Event e) {
+    String databaseName = "N.A.";
+    if (e.getField(Constants.DATABASE_NAME) != null) {
+      databaseName = e.getField(Constants.DATABASE_NAME).toString();
+    }
+    record.setDbName(databaseName);
 
-			Accessor accessor = new Accessor();			
-			accessor.setDataType(Accessor.DATA_TYPE_GUARDIUM_SHOULD_PARSE_SQL);
-			accessor.setLanguage(Constants.LANGUAGE);
-			accessor.setClientHostName(e.getField(Constants.CLIENT_HOST_NAME).toString());
-			accessor.setClientOs(Constants.UNKNOWN_STRING);
-			accessor.setDbUser(e.getField(Constants.User_Name).toString());
-			accessor.setServerType(Constants.SERVER_TYPE_STRING);
-			accessor.setCommProtocol(Constants.UNKNOWN_STRING);
-			accessor.setDbProtocol(Constants.DATA_PROTOCOL_STRING);
-			accessor.setDbProtocolVersion(Constants.UNKNOWN_STRING);
-			accessor.setSourceProgram(e.getField(Constants.APPLICATION_NAME).toString());
-			accessor.setClient_mac(Constants.UNKNOWN_STRING);
-			accessor.setServerDescription(Constants.UNKNOWN_STRING);
+    record.setAppUserName(Constants.APP_USER_NAME);
 
-			String databaseName = Constants.UNKNOWN_STRING;
-			if(e.getField(Constants.DATABASE_NAME) !=null){
-				databaseName=e.getField(Constants.DATABASE_NAME).toString();
-			}
+    record.setTime(Parser.parseTimestamp(e));
 
-			accessor.setServiceName(databaseName);
-			accessor.setServerOs(Constants.UNKNOWN_STRING);
-			accessor.setServerHostName(e.getField(Constants.Server_Hostname).toString());
-			accessor.setOsUser(Constants.UNKNOWN_STRING);		
-		return accessor;
-	}
-	
+    record.setSessionLocator(Parser.parseSessionLocator(e));
+
+    record.setAccessor(Parser.parseAccessor(e));
+
+    if (e.getField(Constants.SUCCEEDED).toString().contains("true")) {
+      Data data = new Data();
+      data.setOriginalSqlCommand(getFieldString(e, Constants.STATEMENT, "N.A."));
+      record.setData(data);
+    } else {
+      ExceptionRecord exceptionRecord = new ExceptionRecord();
+      exceptionRecord.setExceptionTypeId(Constants.SQL_ERROR);
+      exceptionRecord.setDescription(
+          getFieldString(e, Constants.ADDITIONAL_INFORMATION, Constants.UNKNOWN_STRING));
+      exceptionRecord.setSqlString(getFieldString(e, Constants.STATEMENT, "N.A."));
+      record.setException(exceptionRecord);
+    }
+    return record;
+  }
+
+  public static Time parseTimestamp(final Event e) {
+    long date = 0;
+    String dateString = e.getField(Constants.TIMESTAMP).toString();
+    try {
+      date = Long.parseLong(dateString);
+    } catch (NumberFormatException nfe) {
+      // fallback to current time in nanoseconds to avoid failure
+      date = System.currentTimeMillis() * 1_000_000L;
+    }
+    long mini = date / 1000000;
+
+    return new Time(mini, 0, 0);
+  }
+
+  public static SessionLocator parseSessionLocator(final Event e) {
+
+    SessionLocator sessionLocator = new SessionLocator();
+
+    sessionLocator.setClientIp(getFieldString(e, Constants.Client_IP, Constants.DEFAULT_IP));
+    sessionLocator.setClientPort(Constants.DEFAULT_PORT);
+    sessionLocator.setServerIp(Constants.DEFAULT_IP);
+    sessionLocator.setServerPort(Constants.DEFAULT_PORT);
+    sessionLocator.setIpv6(false);
+    sessionLocator.setClientIpv6(Constants.UNKNOWN_STRING);
+    sessionLocator.setServerIpv6(Constants.UNKNOWN_STRING);
+
+    return sessionLocator;
+  }
+
+  public static Accessor parseAccessor(final Event e) {
+
+    Accessor accessor = new Accessor();
+    accessor.setDataType(Accessor.DATA_TYPE_GUARDIUM_SHOULD_PARSE_SQL);
+    accessor.setLanguage(Constants.LANGUAGE);
+    accessor.setClientHostName(
+        getFieldString(e, Constants.CLIENT_HOST_NAME, Constants.UNKNOWN_STRING));
+    accessor.setClientOs(Constants.UNKNOWN_STRING);
+    accessor.setDbUser(getFieldString(e, Constants.User_Name, "N.A."));
+    accessor.setServerType(Constants.SERVER_TYPE_STRING);
+    accessor.setCommProtocol(Constants.UNKNOWN_STRING);
+    accessor.setDbProtocol(Constants.DATA_PROTOCOL_STRING);
+    accessor.setDbProtocolVersion(Constants.UNKNOWN_STRING);
+    accessor.setSourceProgram(getFieldString(e, Constants.APPLICATION_NAME, "N.A."));
+    accessor.setClient_mac(Constants.UNKNOWN_STRING);
+    accessor.setServerDescription(Constants.UNKNOWN_STRING);
+
+    String databaseName = "N.A.";
+    if (e.getField(Constants.DATABASE_NAME) != null) {
+      databaseName = e.getField(Constants.DATABASE_NAME).toString();
+    }
+    accessor.setServiceName(databaseName);
+    accessor.setServerOs(Constants.UNKNOWN_STRING);
+    accessor.setServerHostName(
+        getFieldString(e, Constants.Server_Hostname, Constants.UNKNOWN_STRING));
+    accessor.setOsUser(Constants.UNKNOWN_STRING);
+    return accessor;
+  }
 }
