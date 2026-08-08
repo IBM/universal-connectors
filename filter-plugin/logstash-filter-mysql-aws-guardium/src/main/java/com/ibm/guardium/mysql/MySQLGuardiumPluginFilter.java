f2b86b59c64253b4143cd55ccdff6627c5fa9617
Andy.Chen <andy.chen@ibm.com>
2026-08-08 09:26:30 -0400

    jrubyComplete Version update

--- filter-plugin/logstash-filter-mysql-aws-guardium/src/main/java/com/ibm/guardium/mysql/MySQLGuardiumPluginFilter.java
+++ filter-plugin/logstash-filter-mysql-aws-guardium/src/main/java/com/ibm/guardium/mysql/MySQLGuardiumPluginFilter.java
@@ -29,16 +29,16 @@ public class MySQLGuardiumPluginFilter implements Filter {
 
     public static final String LOG42_CONF = "log4j2uc.properties";
 
-    static {
-        try {
-            String uc_etc = System.getenv("UC_ETC");
-            LoggerContext context = (LoggerContext) LogManager.getContext(false);
-            File file = new File(uc_etc + File.separator + LOG42_CONF);
-            context.setConfigLocation(file.toURI());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    static {
+//        try {
+//            String uc_etc = System.getenv("UC_ETC");
+//            LoggerContext context = (LoggerContext) LogManager.getContext(false);
+//            File file = new File(uc_etc + File.separator + LOG42_CONF);
+//            context.setConfigLocation(file.toURI());
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     private String id;
     public static final PluginConfigSpec<String> SOURCE_CONFIG = PluginConfigSpec.stringSetting("source", "message");
@@ -66,10 +66,10 @@ public class MySQLGuardiumPluginFilter implements Filter {
             log.info(Constants.EVENT_NOW_MYSQL, e.getData());
             try {
 
-                if(null != e.getData()
+                if (null != e.getData()
                         && null != e.getData().get(Constants.COMMAND) && e.getData().get(Constants.COMMAND).toString()
                         .equals(Constants.COMMAND_UNKNOWN) && null != e.getData().get(Constants.DATABASE_USER_NAME)
-                        && e.getData().get(Constants.DATABASE_USER_NAME).toString().equals(Constants.RDS_ADMIN)){
+                        && e.getData().get(Constants.DATABASE_USER_NAME).toString().equals(Constants.RDS_ADMIN)) {
                     continue;
                 }
 
